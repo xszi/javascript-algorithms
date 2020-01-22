8133bb801b5eae0f85c71275a0143fdbf6efa5d0
DESKTOP-JIUHUA\jiuhua-pc <jiuhuaw@126.com>
2020-01-22 16:30:38 +0800

    time compare

--- algorithm/a1/index.js
+++ algorithm/a1/index.js
@@ -1,5 +1,5 @@
 // 使用js求解多项式的和, 例如：f(x) = 10 + 2*x + 3*pow(x, 2)
-let arr = [10, 2, 3], x = 4, n = 2
+let arr = [10, 2, 3], x = 4, n = 10000
 
 function sum(arr, x, n) {
     let result = 0
@@ -9,16 +9,19 @@ function sum(arr, x, n) {
     return result
 }
 
-console.log(sum(arr, x, n))
+console.time()
+sum(arr, x, n)
+console.timeEnd()
 
 // 第二种解法，不断提取公因式
 
-function sum2(arr, x, n) {
-    let result = arr[n]
-    for (let i=n; i>0; i--) {
-        result = arr[i-1] + x*result
-    }
-    return result
-}
-
-console.log(sum2(arr, x, n))
+// function sum2(arr, x, n) {
+//     let result = arr[n]
+//     for (let i=n; i>0; i--) {
+//         result = arr[i-1] + x*result
+//     }
+//     return result
+// }
+// console.time()
+// sum2(arr, x, n)
+// console.timeEnd()
