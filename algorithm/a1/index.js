f6d18f321b5a46a69da552ffa7e3adf327ad300c
DESKTOP-JIUHUA\jiuhua-pc <jiuhuaw@126.com>
2020-01-22 16:21:39 +0800

    algorithm 1

---
+++ algorithm/a1/index.js
@@ -0,0 +1,24 @@
+// 使用js求解多项式的和, 例如：f(x) = 10 + 2*x + 3*pow(x, 2)
+let arr = [10, 2, 3], x = 4, n = 2
+
+function sum(arr, x, n) {
+    let result = 0
+    for (let i=0; i<=n; i++) {
+        result += arr[i]*Math.pow(x, i)
+    }
+    return result
+}
+
+console.log(sum(arr, x, n))
+
+// 第二种解法，不断提取公因式
+
+function sum2(arr, x, n) {
+    let result = arr[n]
+    for (let i=n; i>0; i--) {
+        result = arr[i-1] + x*result
+    }
+    return result
+}
+
+console.log(sum2(arr, x, n))
