044035cc6f8e16d7276514c1221e9b6cf7dde830
DESKTOP-JIUHUA\jiuhua-pc <jiuhuaw@126.com>
2020-05-25 10:22:31 +0800

    滚动到底部监听事件，计算滚动到底部距离的方式

--- algorithm/10_tree.md
+++ algorithm/10_tree.md
@@ -1,4 +1,4 @@
-### 二叉树
+### 二叉树先序遍历
 
 - 递归实现
 
@@ -40,6 +40,46 @@ var preorderTraversal = (root) => {
         if(curNode.left !== null) {
             stack.push(curNode.left)
         }
+    }
+    return list
+}
+```
+
+### 二叉树中序遍历
+
+- 递归实现
+
+```js
+var inorderTraversal = (root) => {
+    let result = []
+    var inorderTraversal = (node) => {
+        if(node) {
+            inorderTraversal(node.left)
+            result.push(node.val)
+            inorderTraversal(node.right)
+        }
+    }
+    inorderTraversal(root)
+    return result
+}
+```
+
+- 迭代实现
+
+```js
+var inorderTraversal = function(root) {
+    let list = []
+    let stack = []
+    let node = root
+    while(stack.length || node) {
+        if(node) {
+            stack.push(node)
+            node = node.left
+            continue
+        }
+        node = stack.pop()
+        list.push(node.val)
+        node = node.right
     }
     return list
 }
---
+++ article/pull-to-bottom.js
@@ -0,0 +1,57 @@
+// 判断滚动条到底部，需要用到DOM的三个属性值，即scrollTop、clientHeight、scrollHeight。
+
+// scrollTop为滚动条在Y轴上的滚动距离。
+
+// clientHeight为内容可视区域的高度。
+
+// scrollHeight为内容可视区域的高度加上溢出（滚动）的距离。
+
+// 从这个三个属性的介绍就可以看出来，滚动条到底部的条件即为scrollTop + clientHeight == scrollHeight。
+
+//1. 滚动条在Y轴上的滚动距离
+function getScrollTop(){
+　　var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+　　if(document.body){
+　　　　bodyScrollTop = document.body.scrollTop;
+　　}
+　　if(document.documentElement){
+　　　　documentScrollTop = document.documentElement.scrollTop;
+　　}
+　　scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+　　return scrollTop;
+}
+
+//2. 文档的总高度
+function getScrollHeight(){
+　　var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+　　if(document.body){
+　　　　bodyScrollHeight = document.body.scrollHeight;
+　　}
+　　if(document.documentElement){
+　　　　documentScrollHeight = document.documentElement.scrollHeight;
+　　}
+　　scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+　　return scrollHeight;
+}
+
+//3. 浏览器视口的高度
+function getWindowHeight(){
+　　var windowHeight = 0;
+　　if(document.compatMode == "CSS1Compat"){
+　　　　windowHeight = document.documentElement.clientHeight;
+　　}else{
+　　　　windowHeight = document.body.clientHeight;
+　　}
+　　return windowHeight;
+}
+
+//  4. 全部监听滚动或者 组件上监听@scroll
+window.onscroll = function(){
+　　if(getScrollTop() + getWindowHeight() == getScrollHeight()){
+　　　　alert("you are in the bottom!");
+　　}
+};
+
+
+// 注：记得这里踩过一个坑，关于全局监听和局部监听，但是具体是什么想不起来了，下次遇到再来补充！
+
