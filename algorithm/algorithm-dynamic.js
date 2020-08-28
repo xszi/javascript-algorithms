e5116b6c0beb3d2b74a9cb4b42ffa48b04fc87b4
xszi <jiuhuaw@126.com>
2020-08-28 15:26:07 +0800

    merge sort algorithm

--- algorithm/algorithm-dynamic.js
+++ algorithm/algorithm-dynamic.js
@@ -71,22 +71,73 @@
 
 // 快速排序
 
+// const sortArr = [7, 4, 1, 3, 5, 10, 6, 9, 8, 2]
+
+// const quickSort = (arr) => {
+//     if (arr.length < 2) return arr
+//     let leftArr = []
+//     let rightArr = []
+//     let pivot = arr[0]
+//     arr.forEach(element => {
+//         if (element < pivot) {
+//             leftArr.push(element)
+//         } else if (element > pivot) { 
+//             // 必须要加 if (element > pivot), 排除相等情况落入右边，引起Maximum call stack size exceeded
+//             rightArr.push(element)
+//         }
+//     })
+//     return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+// }
+
+// console.log(quickSort(sortArr));
+
+// 归并排序
+
 const sortArr = [7, 4, 1, 3, 5, 10, 6, 9, 8, 2]
 
-const quickSort = (arr) => {
-    if (arr.length < 2) return arr
-    let leftArr = []
-    let rightArr = []
-    let pivot = arr[0]
-    arr.forEach(element => {
-        if (element < pivot) {
-            leftArr.push(element)
-        } else if (element > pivot) { 
-            // 必须要加 if (element > pivot), 排除相等情况落入右边，引起Maximum call stack size exceeded
-            rightArr.push(element)
+const merge = (left, right) => {
+    let result = []
+    let iLeft = 0
+    let iRight = 0
+    while (iLeft < left.length && iRight < right.length) {
+        if (left[iLeft] < right[iRight]) {
+            result.push(left[iLeft])
+            iLeft++
+        } else {
+            result.push(right[iRight])
+            iRight++
         }
-    })
-    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)]
+    }
+    while (iLeft < left.length) {
+        result.push(left[iLeft])
+        iLeft++
+    }
+    while (iRight < right.length) {
+        result.push(right[iRight])
+        iRight++
+    }
+    return result
 }
 
-console.log(quickSort(sortArr));
+const mergeSort = (arr) => {
+    if (arr.length < 2) return arr
+    let left = []
+    let right = []
+    for (let i = 0, len = arr.length; i < len; i++) {
+        if (i < len/2 ) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+console.log(mergeSort(sortArr));
+// const mergeArr1 = [1,5]
+// const mergeArr2 = [2]
+// console.log(merge(mergeArr1, mergeArr2))
+
+// 64匹马问题 --> 9匹马问题
+
+// 截取文件读写，脚本书写测试 --> 自动化
