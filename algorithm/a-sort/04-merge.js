9564f5e4fc49722cc6f6072e2c4349b86b111a53
xszi <jiuhuaw@126.com>
2020-08-31 19:32:23 +0800

    the second method of merge

--- algorithm/a-sort/04-merge.js
+++ algorithm/a-sort/04-merge.js
@@ -3,33 +3,60 @@
 
 const sortArr = [7, 4, 1, 3, 5, 10, 6, 9, 8, 2]
 
-const merge = (left, right) => {
-    let result = []
-    let iLeft = 0
-    let iRight = 0
+// merge 的第一种写法
+// const merge = (left, right) => {
+//     let result = []
+//     let iLeft = 0
+//     let iRight = 0
 
      //left, right本身肯定都是从小到大排好序的
-    while (iLeft < left.length && iRight < right.length) {
-        if (left[iLeft] < right[iRight]) {
-            result.push(left[iLeft])
-            iLeft++
-        } else {
-            result.push(right[iRight])
-            iRight++
-        }
-    }
+    // while (iLeft < left.length && iRight < right.length) {
+    //     if (left[iLeft] < right[iRight]) {
+    //         result.push(left[iLeft])
+    //         iLeft++
+    //     } else {
+    //         result.push(right[iRight])
+    //         iRight++
+    //     }
+    // }
 
     //不可能同时存在left和right都有剩余项的情况,
     // 要么left要么right有剩余项, 把剩余项加进来即可
-    while (iLeft < left.length) {
-        result.push(left[iLeft])
-        iLeft++
+//     while (iLeft < left.length) {
+//         result.push(left[iLeft])
+//         iLeft++
+//     }
+//     while (iRight < right.length) {
+//         result.push(right[iRight])
+//         iRight++
+//     }
+//     return result
+// }
+
+// merge 的第二种写法
+const merge = (left, right) => {
+    let mergedArr = []
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            mergedArr.push(left[0])
+            left.shift()
+        } else {
+            mergedArr.push(right[0])
+            right.shift()
+        }
     }
-    while (iRight < right.length) {
-        result.push(right[iRight])
-        iRight++
+
+    while (left.length) {
+        mergedArr.push(left[0])
+        left.shift()
     }
-    return result
+
+    while (right.length) {
+        mergedArr.push(right[0])
+        right.shift()
+    }
+
+    return mergedArr
 }
 
 const mergeSort = (arr) => {
@@ -47,7 +74,7 @@ const mergeSort = (arr) => {
 }
 
 console.log(mergeSort(sortArr));
-// const mergeArr1 = [1,5]
+// const mergeArr1 = [1, 5]
 // const mergeArr2 = [2]
 // console.log(merge(mergeArr1, mergeArr2))
 
