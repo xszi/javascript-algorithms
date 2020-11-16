27466d65b6c4e11e9b27fa34338f6f80a01a90e1
xszi <jiuhuaw@126.com>
2020-11-16 17:40:00 +0800

    双向链表

--- algorithm/e-linkedList/01-双向链表.js
+++ algorithm/e-linkedList/01-双向链表.js
@@ -16,7 +16,8 @@ function DoubleLinkedList () {
                 current = head,
                 previous,
                 index = 0
-
+            
+            // 在头部加入
             if (position === 0) {
                 if (!head) {
                     head = node
@@ -26,7 +27,7 @@ function DoubleLinkedList () {
                     current.prev = node
                     head = node
                 } 
-            } else if (position === length) {
+            } else if (position === length) { // 在尾部加入
                 current = tail
                 current.next = node
                 node.prev = current
@@ -44,6 +45,45 @@ function DoubleLinkedList () {
             return true
         } else {
             return false
+        }
+    }
+
+    this.removeAt = function(position) {
+        // 检查越界
+        if (position > -1 && position < length) {
+            let current = head
+                previous
+                index = 0
+
+            // 移除第一项
+            if (position === 0) {
+                head = current.next
+
+                // 如果只有一项，更新tail 
+                if (length === 1) {
+                    tail = null
+                } else {
+                    head.prev = null
+                }
+            } else if (position === length-1) { // 最后一项
+                current = tail
+                tail = current.prev
+                tail.next = null
+            } else {
+                while (index++ < position) {
+                    previous = current
+                    current = current.next
+                }
+
+                previous.next = current.next
+                current.next.prev = previous
+            }
+            
+            length--
+
+            return current.element
+        } else {
+            return null
         }
     }
 }
