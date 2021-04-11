8ba13328f6ca7189d7c601d0486fa366c62263d5
xszi <jiuhuaw@126.com>
2021-04-11 16:28:16 +0800

    array

--- algorithm/b-array/0-test.js
+++ algorithm/b-array/0-test.js
@@ -1,36 +1,39 @@
-const threeSum = (nums) => {
-    if (nums.length < 3) return []
-    // 先排序
-    nums = nums.sort((a, b) => a - b)
-    let result = []
-    for(let i = 0; i < nums.length - 2; i++) {
-        // 排序后第一项大于零，后面项都大于零
-        if (nums[i] > 0) break
-        // 去重
-        if (nums[i] === nums[i - 1]) continue
-        let left = i + 1
-        let right = nums.length - 1
-        while(left < right) {
-            let sum = nums[i] + nums[left] + nums[right]
-            if (sum === 0) {
-                result.push([nums[i], nums[left++], nums[right--]])
-                // push 加了之后防止还存在重复
-                while(nums[left] === nums[left - 1]){
-                    left++
-                }
-                while(nums[right] === nums[right + 1]){
-                    right--
-                }
-            } else if (sum > 0) {
-                right--
-            } else {
-                left++
-            }
+// `[10, 3, 2, 5, 8, 2, 2, 7]` 传入 `n = 4`
+// [7, 3, 2, 5, 8, 2, 2, 10] flag = 10
+// [7, 3, 2, 5, 8, 2, 2] flag = 7
+// [2, 3, 2, 5, 8, 2, 7]
+// [2, 3, 2, 5, 7, 2, 8]
+var findNthNumber = function (array, n) {
+    return partition(array, n, 0, array.length - 1);
+}
+var partition = function (nums, n, start, end) {
+    var i = start;
+    var j = end;
+    // 选择的对比的元素，比 flag 小的元素放左边，比 flag 大的元素放右边
+    var flag = nums[start];
+    while (start < end) {
+        while (start < end && nums[end] >= flag) {
+            // 先遍历右侧元素，所有大于 flag 的元素先不动
+            end--;
         }
+        swap(nums, start, end);
+        // 此时，右侧元素 nums[j] 已经小于 flag 了
+        while (start < end && nums[start] <= flag) {
+            // 遍历左侧元素，所有小于 flag 的元素不动
+            start++;
+        }
+        swap(nums, start, end); // 交换一下当前的两个位置不对的数字
     }
-    return result
+    if (start === n) return flag;
+    if (start > n) return partition(nums, n, i, start - 1); // 递归左侧
+    return partition(nums, n, start + 1, j); // 递归右侧
+}
+// 交换数组中的 i 和 j 下标的两个元素
+var swap = function (array, i, j) {
+    var temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
 }
 
-const nums = [-1, 0, 1, 2, -1, -4]
-
-console.log(threeSum(nums));
+const nums = [2, 3, 2, 5, 7, 2, 8]
+console.log(findNthNumber(nums)); 
--- algorithm/b-array/03-三数之和.js
+++ algorithm/b-array/03-三数之和.js
@@ -1,94 +1,41 @@
-[-2, -1, 0, 1, 2]
 // 解题步骤：
 // 第一步：找临界条件
 // 第二步：找特殊情况
 // 第三步：一般性的算法
 // 第四步：回归分析检查
 const threeSum = (nums) => {
-    // 特殊情况一：数组不存在或长度小于3
-    if (!nums || nums.length < 3) {
-        return []
-    }
-    let result = []
+    if (nums.length < 3) return []
     // 先排序
-    nums.sort((a, b) => a - b)
-    for(let i=0; i<nums.length; i++) {
-        // // 特殊情况2：num[i]为相加项的第一项，因为数组已排序，所以如果第一项大于0，相加结果必大于0,退出遍历
+    nums = nums.sort((a, b) => a - b)
+    let result = []
+    for(let i = 0; i < nums.length - 2; i++) {
+        // 排序后第一项大于零，后面项都大于零
         if (nums[i] > 0) break
         // 去重
-        if (i > 0 && nums[i] ===  nums[i-1]) continue
-        // 使用快慢指针夹逼紧凑
-        let second = i + 1 // 第二项
-        let last = nums.length - 1 // 最后一项
-        // 临界条件 最后项索引小于第二项索引
-        while (second < last) {
-            const sum = nums[i] + nums[second] + nums[last]
-            if (!sum) {
-                // 如果找到push进结果数组
-                result.push([nums[i], nums[second], nums[last]])
-                // 去重
-                while(nums[second] === nums[second + 1]) second++;
-                while(nums[last] === nums[last - 1]) last--
-                // 前后指针移动
-                second++;
-                last--
+        if (nums[i] === nums[i - 1]) continue
+        let left = i + 1
+        let right = nums.length - 1
+        while(left < right) {
+            let sum = nums[i] + nums[left] + nums[right]
+            if (sum === 0) {
+                result.push([nums[i], nums[left++], nums[right--]])
+                // push 加了之后防止还存在重复
+                while(nums[left] === nums[left - 1]){
+                    left++
+                }
+                while(nums[right] === nums[right + 1]){
+                    right--
+                }
+            } else if (sum > 0) {
+                right--
+            } else {
+                left++
             }
-            // sum < 0 向右移动第二项指针
-            else if(sum < 0) second++;
-            // sum > 0 向左移动最后项指针
-            else if(sum > 0) last++;
         }
     }
     return result
 }
 
+const nums = [-1, 0, 1, 2, -1, -4]
 
-//这道题经典的是细节，需要考虑蛮多细节的
-//解法：
-//1.暴力破解，三层枚举，O（n^3）效率太低不考虑
-//2.a+b+c = 0，转换思路，a+b = -c，这不就是两数之和嘛？
-//3.利用双指针夹逼，但是怎么避免重复呢？
-//3.1 排序即可，利用排序好的数组，固定一个指针i，夹逼选举left和right
-//3.2 这道题难度在于要考虑的细节太多，多想想即可。
-//3.3 扩展一下，如果是4数之和，五数之和，N数之和呢？怎么解决？
-const threeSum = (nums) => {
-    const len = nums.length
-    const result = []
-    // 因为是三数之和，小于三个不用考虑了
-    if(len < 3){
-      return result
-    }
-    nums.sort((a, b) => a - b)
-    // len - 2 同样道理，小于三个不用考虑
-    for(let i = 0; i < len - 2; i++){
-      // 如果第一个就大于0了，三个加起来肯定大于0
-      if(nums[i] > 0){
-        break
-      }
-      // 避免掉重复的情况
-      if(i && nums[i] === nums[i - 1]){
-         continue
-      }
-      let left = i + 1
-      let right = len - 1
-      // 双指针夹逼
-      while(left < right){
-        const sum = nums[i] + nums[left] + nums[right]
-        if(sum === 0){
-           result.push([nums[i], nums[left++], nums[right--]])
-           // push 加了之后防止还存在重复
-           while(nums[left] === nums[left - 1]){
-             left++
-           }
-           while(nums[right] === nums[right + 1]){
-             right--
-           } 
-        }else if(sum > 0){
-            right--
-        }else{
-            left++
-        }
-      }
-    }
-    return result
-  }
+console.log(threeSum(nums));
