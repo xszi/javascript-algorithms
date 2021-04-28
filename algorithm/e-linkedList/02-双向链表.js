92eb4af842675ff9957401cb8540187e85bc9d5a
xszi <jiuhuaw@126.com>
2021-04-28 17:25:12 +0800

    doubleLinkedList

--- algorithm/e-linkedList/02-双向链表.js
+++ algorithm/e-linkedList/02-双向链表.js
@@ -1,5 +1,5 @@
-function DoubleLinkedList () {
-    let Node = function(element) {
+function DoubleLinkedList() {
+    let Node = function (element) {
         this.element = element
         this.next = null
         this.prev = null
@@ -9,14 +9,14 @@ function DoubleLinkedList () {
     let head = null
     let tail = null
 
-    this.insert = function(position, element) {
+    this.insert = function (position, element) {
         // 检查越界
-        if(position >= 0 && position <= length) {
+        if (position >= 0 && position <= length) {
             let node = new Node(element),
                 current = head,
                 previous,
                 index = 0
-            
+
             // 在头部加入
             if (position === 0) {
                 if (!head) {
@@ -26,12 +26,12 @@ function DoubleLinkedList () {
                     node.next = current
                     current.prev = node
                     head = node
-                } 
+                }
             } else if (position === length) { // 在尾部加入
                 current = tail
                 current.next = node
                 node.prev = current
-                tail = node      
+                tail = node
             } else {
                 while (index++ < position) {
                     previous = current
@@ -48,12 +48,12 @@ function DoubleLinkedList () {
         }
     }
 
-    this.removeAt = function(position) {
+    this.removeAt = function (position) {
         // 检查越界
         if (position > -1 && position < length) {
             let current = head
-                previous
-                index = 0
+            previous
+            index = 0
 
             // 移除第一项
             if (position === 0) {
@@ -65,7 +65,7 @@ function DoubleLinkedList () {
                 } else {
                     head.prev = null
                 }
-            } else if (position === length-1) { // 最后一项
+            } else if (position === length - 1) { // 最后一项
                 current = tail
                 tail = current.prev
                 tail.next = null
@@ -78,9 +78,104 @@ function DoubleLinkedList () {
                 previous.next = current.next
                 current.next.prev = previous
             }
-            
+
             length--
 
+            return current.element
+        } else {
+            return null
+        }
+    }
+}
+
+class DoubleLinkedNode {
+    constructor(element) {
+        this.element = element
+        this.prev = null
+        this.next = null
+    }
+}
+
+class DoubleLinkedList {
+    constructor() {
+        this.length = 0
+        this.head = null
+        this.tail = null
+    }
+
+    insert(position, element) {
+        // 检查越界
+        if (position >= 0 && position <= length) {
+            let node = new DoubleLinkedNode(element)
+            current = this.head
+            previous
+            index = 0
+
+            // 头部加入
+            if (position === 0) {
+                if (!this.head) {
+                    this.head = node
+                    this.tail = node
+                } else {
+                    node.next = current
+                    current.prev = node
+                    this.head = node
+                }
+            } else if (position === length) {
+                // 尾部加入
+                current = this.tail
+                current.next = node
+                node.prev = current
+                this.tail = node
+            } else {
+                // 中间加入
+                while (index++ <position) {
+                    previous = current
+                    current = current.next
+                }
+
+                previous.next = node
+                node.prev = previous
+                node.next = current
+                current.prev = node
+            }
+            length++
+            return true
+        } else {
+            return false
+        }
+    }
+
+    removeAt (position) {
+        // 检查边界
+        if (position > -1 && position < length) {
+            let current = this.head
+                previous
+                index = 0
+
+            // 移除第一项
+            if (position === 0) {
+                this.head = current.next
+                // 如果只有一项，更新tail
+                if(this.length === 1) {
+                    this.tail = null
+                } else {
+                    this.head.prev = null
+                }
+            } else if (position === length - 1) {
+                // 移除最后一项
+                current = this.tail
+                this.tail = current.prev
+                this.tail.next = null
+            } else {
+                while (index++ < position) {
+                    previous = current
+                    current = current.next
+                }
+                previous.next = current.next
+                current.next.prev = previous
+            }
+            length--
             return current.element
         } else {
             return null
